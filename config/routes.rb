06a64bc7051e160efbc93d71b0946b82b6bca406
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-27 14:37:06 -0500

    added create controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/homes", controller: "homes", action: "index"
   get "/homes/:id", controller: "homes", action: "show"
+  post "/homes", controller: "homes", action: "create"
 end
